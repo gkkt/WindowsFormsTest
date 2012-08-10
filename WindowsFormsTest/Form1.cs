f258d97c6352adb6bd0eeb2c9859f44dc8f36316
Grzegorz Kusiak <grzegorz.kusiak@gmail.com>
2012-08-10 14:22:44 +0200

    MessageBox - Ala ma kota

--- WindowsFormsTest/Form1.cs
+++ WindowsFormsTest/Form1.cs
@@ -15,5 +15,10 @@ namespace WindowsFormsTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ala ma kota");
+        }
     }
 }
